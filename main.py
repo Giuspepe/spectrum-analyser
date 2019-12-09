057047dddcfec4bccb9489858d46c12f42eb549a
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2019-12-09 22:25:05 +0100

    es funktioniert!!!!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import board
 import time
 import neopixel
@@ -35,26 +36,26 @@ class LedStripe(threading.Thread):
 
     def run(self):
         #self.sinus(100)
-            for increment in range(0, 1000):
+           while True:
                 #for index in range(0, 8):
                 #    self.value[index] = random.randint(0, 43)  # grab new FFT values
                 self.fallingDot()
                 self.refreshStripe()
-                time.sleep(0.1)
+              #  time.sleep(0.1)
 
     def refreshStripe(self):
         for column in range(0, num_columns):
-            for neglevel in range(0, num_rows - spectrum_levels[column]):
+            for neglevel in range(0, num_rows - int(spectrum_levels[column])):
                 self.pixels[num_rows * column + num_rows - 1 - neglevel] = (0, 0, 0)
-            for level in range(0, spectrum_levels[column]):
+            for level in range(0, int(spectrum_levels[column])):
                 self.pixels[num_rows * column + level] = levelColor
             self.pixels[num_rows * column + self.oldValue[column]] = dotColor
         self.pixels.show()
 
     def fallingDot(self):
         for column in range(0, num_columns):
-            if self.oldValue[column] <= spectrum_levels[column]:
-                self.oldValue[column] = spectrum_levels[column]
+            if self.oldValue[column] <= int(spectrum_levels[column]):
+                self.oldValue[column] = int(spectrum_levels[column])
             elif self.oldValue[column] > 0 and self.dotfallingrate > 2:
                 self.oldValue[column] -= 1
         if self.dotfallingrate > 2:
@@ -90,7 +91,7 @@ class FFT(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         logging.basicConfig(level=logging.DEBUG)
-        self.chunk_size = 4096  # Use a multiple of 8 (FFT will compute faster with a multiple of 8)
+        self.chunk_size = 1024  # Use a multiple of 8 (FFT will compute faster with a multiple of 8)
         py_audio = pyaudio.PyAudio()
         device_index = 0
         device_info = py_audio.get_device_info_by_index(device_index)
@@ -103,7 +104,7 @@ class FFT(threading.Thread):
         self.frequency_bins = [(0, 156), (156, 313), (313, 625), (625, 1250), (1250, 2500), (2500, 5000), (5000, 10000),
                           (10000, 20000)]  # Lower and upper frequencies of each frequency bin
 
-        self.max_value_transformed_fft_data = 60
+        self.max_value_transformed_fft_data = 50
         self.weighting = [2, 8, 8, 16, 16, 32, 32, 64]  # scaling factors of frequency bins. rule of thumb: double frequency -> half power
 # Return power array index corresponding to a particular frequency
     def get_power_array_index_of_frequency(self,frequency):
@@ -111,26 +112,26 @@ class FFT(threading.Thread):
 
 
     def run(self):
-        global spectrum_levels
-        audio_data = self.audio_stream.read(self.chunk_size, exception_on_overflow=False)
-        audio_data = unpack("%dh" % (len(audio_data) / 2), audio_data)
-        audio_data = np.array(audio_data, dtype='h')
-        # apply fft
-        fft_data = np.fft.rfft(audio_data)
-        # remove last elementin array to make it the same size as the chunk
-        fft_data = np.delete(fft_data, len(fft_data) - 1)
-        # transform complex fft data to real data
-        fft_data = np.abs(fft_data)
-        # divide fft data into the frequency bins
-        for i, frequency_bin in enumerate(self.frequency_bins):
-            low_index = self.get_power_array_index_of_frequency(frequency_bin[0])
-            high_index = self.get_power_array_index_of_frequency(frequency_bin[1])
-            spectrum_levels[i] = np.mean(fft_data[low_index:high_index])  # TODO: use bell curve instead of mean (Flori)
+        while True:
+            global spectrum_levels
+            audio_data = self.audio_stream.read(self.chunk_size, exception_on_overflow=False)
+            audio_data = unpack("%dh" % (len(audio_data) / 2), audio_data)
+            audio_data = np.array(audio_data, dtype='h')
+            # apply fft
+            fft_data = np.fft.rfft(audio_data)
+            # remove last elementin array to make it the same size as the chunk
+            fft_data = np.delete(fft_data, len(fft_data) - 1)
+            # transform complex fft data to real data
+            fft_data = np.abs(fft_data)
+            # divide fft data into the frequency bins
+            for i, frequency_bin in enumerate(self.frequency_bins):
+                low_index = self.get_power_array_index_of_frequency(frequency_bin[0])
+                high_index = self.get_power_array_index_of_frequency(frequency_bin[1])
+                spectrum_levels[i] = np.mean(fft_data[low_index:high_index])  # TODO: use bell curve instead of mean (Flori)
 
-        # tidy up spectrum level values
-        spectrum_levels = np.divide(np.multiply(spectrum_levels, self.weighting), 1000000)  # TODO: 1e6 determined empirically
-        spectrum_levels = np.interp(spectrum_levels, [0, self.max_value_transformed_fft_data],  [0, num_rows])  # TODO: max_value_transformed_fft_data was determined empirically
-        spectrum_levels = list(map(int, spectrum_levels))
+            # tidy up spectrum level values
+            spectrum_levels = np.divide(np.multiply(spectrum_levels, self.weighting), 1000000)  # TODO: 1e6 determined empirically
+            spectrum_levels = np.interp(spectrum_levels, [0, self.max_value_transformed_fft_data],  [0, num_rows]) # TODO: max_value_transformed_fft_data was determined empirically
 
 def main():
     try:
@@ -138,9 +139,6 @@ def main():
         stripe.start()
         FFTlocal = FFT()
         FFTlocal.start()
-        while True:
-            print("jet")
-            time.sleep(1)
 
     except Exception as e:
         print("EXCEPTION CATCHED")
